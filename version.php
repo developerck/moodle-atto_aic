fc6884c73c6a727ea0c0e50e203383b7eccb770a
Chandra Kishor <developerck@users.noreply.github.com>
2024-01-23 18:18:29 +0530

    Update version.php

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'atto_aic';
 $plugin->release = '0.1.0';
-$plugin->version = 2023062200;
+$plugin->version = 2023062201;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_ALPHA;
